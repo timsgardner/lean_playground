79e552db49c97b493f2e7c3c1be42139bc3993bf
timsgardner <451342+timsgardner@users.noreply.github.com>
2026-06-30 19:34:51 -0400

    Completed exercises for quantifier logic

--- LeanPlayground/Scratch/Exercises_01.lean
+++ LeanPlayground/Scratch/Exercises_01.lean
@@ -254,66 +254,100 @@ theorem quant1
     (h : ∀ x : α, P x)
     (a : α) :
     P a := by
-  sorry
+  exact h a
 
 -- 2. Prove a universal statement.
 theorem quant2
     (h : ∀ x : α, P x → Q x) :
     (∀ x : α, P x) → ∀ x : α, Q x := by
-  sorry
+  intro px x
+  have pxqx := h x
+  apply pxqx
+  exact px x
 
 -- 3. Universal distributes over conjunction, forward direction.
 theorem quant3 :
     (∀ x : α, P x ∧ Q x) → (∀ x : α, P x) ∧ (∀ x : α, Q x) := by
-  sorry
+  intro pxqx
+  constructor
+  · intro x
+    exact (pxqx x).left
+  · intro x
+    exact (pxqx x).right
 
 -- 4. Universal distributes over conjunction, backward direction.
 theorem quant4 :
     (∀ x : α, P x) ∧ (∀ x : α, Q x) → ∀ x : α, P x ∧ Q x := by
-  sorry
+  rintro ⟨u_px, u_qx⟩
+  intro x
+  exact ⟨u_px x, u_qx x⟩
 
 -- 5. Existential gives a witness and a proof.
 theorem quant5 :
     (∃ x : α, P x ∧ Q x) → ∃ x : α, P x := by
-  sorry
+  intro e_px_qx
+  rcases e_px_qx with ⟨x, ⟨px, qx⟩⟩
+  exists x
 
 -- 6. Existential distributes over disjunction, forward direction.
 theorem quant6 :
     (∃ x : α, P x ∨ Q x) → (∃ x : α, P x) ∨ (∃ x : α, Q x) := by
-  sorry
+  intro e_px_qx
+  rcases e_px_qx with ⟨x, px | qx⟩
+  left
+  · exists x
+  right
+  · exists x
 
 -- 7. Existential distributes over disjunction, backward direction.
 theorem quant7 :
     (∃ x : α, P x) ∨ (∃ x : α, Q x) → ∃ x : α, P x ∨ Q x := by
-  sorry
+  intro stuff
+  rcases stuff with e_px | e_qx
+  · rcases e_px with ⟨x, px⟩
+    exists x
+    exact Or.inl px
+  · rcases e_qx with ⟨x, qx⟩
+    exists x
+    exact Or.inr qx
+
 
 -- 8. Move a universal implication across an existential.
 theorem quant8
     (h : ∀ x : α, P x → Q x) :
     (∃ x : α, P x) → ∃ x : α, Q x := by
-  sorry
+  intro e_px
+  obtain ⟨x, px⟩ := e_px
+  exists x
+  exact (h x) px
 
 -- 9. Reuse the same witness.
 theorem quant9
     (h : ∃ x : α, P x ∧ Q x) :
     ∃ x : α, Q x ∧ P x := by
-  sorry
+  obtain ⟨x, ⟨px, qx⟩⟩ := h
+  exists x
 
 -- 10. Swap order of universal quantifiers.
 theorem quant10 :
     (∀ x : α, ∀ y : β, T x y) → ∀ y : β, ∀ x : α, T x y := by
-  sorry
+  rintro hi there marty
+  exact hi marty there
 
 -- 11. Swap order of existential quantifiers.
 theorem quant11 :
     (∃ x : α, ∃ y : β, T x y) → ∃ y : β, ∃ x : α, T x y := by
-  sorry
+  rintro ⟨x, y, txy⟩
+  exists y
+  exists x
 
 -- 12. Universal plus existential.
 theorem quant12
     (h : ∀ x : α, P x → Q x)
     (ex : ∃ x : α, P x) :
     ∃ x : α, Q x := by
-  sorry
+  rcases ex with ⟨x, px⟩
+  exists x
+  exact (h x) px
 
 end QuantifierLogic
